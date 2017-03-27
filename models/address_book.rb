b6ec9248ebadf0c8b904032274aebeb1da3ec208
tmoolmuang <tmoolmuang@yahoo.com>
2017-03-27 08:32:46 -0700

    checkpoint08-assignment: completed

--- models/address_book.rb
+++ models/address_book.rb
@@ -47,4 +47,13 @@ class AddressBook
     return nil
   end
   
+  def iterative_search(name)
+    @entries.each do |entry|
+      if name == entry.name
+        return entry
+      end
+    end
+    return nil
+  end
+  
 end
